9791067a227d09e30c329f2bc5f30a1889f4c78b
Markus Müller <markus.mueller.1.g@googlemail.com>
2020-03-22 21:57:14 +0100

    deduplicated matagorda bashrc

--- bashrc_common.sh
+++ bashrc_common.sh
@@ -1,9 +1,63 @@
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
+
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+#shopt -s globstar
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
 export EDITOR=nvim
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/configfiles/bash_aliases.sh ]; then
+    . ~/configfiles/bash_aliases.sh
+fi
+
 # find hostspecific configuration_file
 hcp=~/configfiles/bash_$(hostname).sh
 test=$(hostname)
 if [ -f "$hpc" ]; then
 	source $hcp
 fi
-echo "####################\n$test"
-#
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+    alias vi="nvim"
+    source /home/mm/.nix-profile/etc/profile.d/nix.sh 
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+    alias cdc="cd  ~/bgc-md/prototypes/ModelsAsScriptsWithSpecialVarNames/models/miniCable/cable_all/cable_transit_time"
+fi
