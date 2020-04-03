e481b8cd33453034dbd6455144452bb4789617bd
Markus Müller <markus.mueller.1.g@googlemail.com>
2020-04-03 17:59:16 +0200

    fixed the missing colors in the ls command

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -2,5 +2,14 @@ alias vi='nvim'
 alias gt='gnome-terminal'
 alias jls='jupyter-lab --port=8889 --port-retries=0'
 alias ll='ls -alF'
-alias ls='ls -A'
 alias l='ls -CF'
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    alias dir='dir --color=auto'
+    alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
