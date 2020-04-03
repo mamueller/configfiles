fd69bb1e09876b4cc1dd4b5dd28ca82a5175a623
Markus Müller <markus.mueller.1.g@googlemail.com>
2020-04-03 18:06:58 +0200

    avoid browser start on remote jupyter server

--- bashrc_georgia.sh
+++
@@ -1,4 +0,0 @@
-alias R-devel='/home/mm/local/lib/R-devel/bin/R'
-alias Rscript-devel='/home/mm/local/lib/R-devel/bin/Rscript'
-alias cdp="cd /home/mm/bgc-md/prototypes/ModelsAsScriptsWithSpecialVarNames"
-
